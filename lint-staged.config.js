dc0705e89c16ea2022c1cdfc0ae77a206c615463
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-18 11:48:46 +0100

    TS Config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{ts,tsx}': ['eslint --fix'],
+  '*.{ts,tsx}': ['eslint --fix', 'tsc'],
   '*.{ts,tsx,js,css,json,md,html,yml}': ['prettier --write'],
 }
--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -4,6 +4,8 @@ import * as azure from '@pulumi/azure'
 // Create an Azure Resource Group
 const resourceGroup = new azure.core.ResourceGroup('resourceGroup')
 
+console.log('hello')
+
 // Create an Azure resource (Storage Account)s
 const account = new azure.storage.Account('storage', {
   // The location for the storage account will be derived automatically from the resource group.
