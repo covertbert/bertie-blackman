7842dbd824746e6c11c2321f087bf10a4075cb68
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-18 19:09:23 +0100

    Storage account with Pulumi

--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -2,15 +2,37 @@ import * as pulumi from '@pulumi/pulumi'
 import * as azure from '@pulumi/azure'
 
 // Create an Azure Resource Group
-const resourceGroup = new azure.core.ResourceGroup('resourceGroup')
+const resourceGroup = new azure.core.ResourceGroup('staging-rg', {
+  location: azure.Locations.WestEurope,
+})
 
 // Create an Azure resource (Storage Account)s
-const account = new azure.storage.Account('storage', {
+const storageAccount = new azure.storage.Account('staging', {
   // The location for the storage account will be derived automatically from the resource group.
   resourceGroupName: resourceGroup.name,
   accountTier: 'Standard',
+  accountKind: 'StorageV2',
   accountReplicationType: 'LRS',
 })
 
-// Export the connection string for the storage account
-export const connectionString = account.primaryConnectionString
+export const staticEndpoint = storageAccount.primaryWebEndpoint
+
+// We can add a CDN in front of the website
+const cdn = new azure.cdn.Profile('staging-cdn', {
+  resourceGroupName: resourceGroup.name,
+  sku: 'Standard_Microsoft',
+})
+
+const endpoint = new azure.cdn.Endpoint('staging-cdn-ep', {
+  resourceGroupName: resourceGroup.name,
+  profileName: cdn.name,
+  originHostHeader: storageAccount.primaryWebHost,
+  origins: [
+    {
+      name: 'blobstorage',
+      hostName: storageAccount.primaryWebHost,
+    },
+  ],
+})
+
+export const cdnEndpoint = pulumi.interpolate`https://${endpoint.hostName}/`
