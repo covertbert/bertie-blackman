535f5c5b9eca555d1549b405115b1a28863689ad
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-23 22:10:07 +0100

    Adds head attributes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{ts,tsx}': ['eslint --fix', 'tsc --noEmit'],
+  '*.{ts,tsx}': ['eslint --fix'],
   '*.{ts,tsx,js,css,json,md,html,yml}': ['prettier --write'],
 }
--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
-const App = () => <div>Hello world!</div>
+const App = () => (
+  <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>My Title</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+      <html lang="en" />
+    </Helmet>
+    Hello world!
+  </div>
+)
 
 export default App
