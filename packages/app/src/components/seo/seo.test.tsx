2cf9b520a62ed7efe1d64f1d97f9d51c130b7563
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-24 12:13:51 +0100

    Adds Jest

---
+++ packages/app/src/components/seo/seo.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import SEO from './seo.component'
+
+describe('it', () => {
+  it('tests', () => {
+    render(<SEO />)
+  })
+})
