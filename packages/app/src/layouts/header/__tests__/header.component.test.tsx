5af56e8bed24cd29711a4b2f5e97c52770de14c2
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-27 20:11:20 +0100

    Adds tests for header functionality

--- packages/app/src/layouts/header/__tests__/header.component.test.tsx
+++ packages/app/src/layouts/header/__tests__/header.component.test.tsx
@@ -4,13 +4,24 @@ import { render, fireEvent } from '@testing-library/react'
 import Header from '../header.component'
 
 describe('Header', () => {
-  it('opens the mobile nav when the button is clicked', () => {
+  it('opens and closes the mobile nav when the "Menu" button is clicked', () => {
     const { getByText } = render(<Header />)
 
     const menuButton = getByText('Menu')
-
     fireEvent.click(menuButton)
 
     expect(getByText('Close')).toBeTruthy()
+  })
+
+  it('closes the mobile nav when the "Close" button is clicked', () => {
+    const { getByText } = render(<Header />)
+
+    const menuButton = getByText('Menu')
+    fireEvent.click(menuButton)
+
+    const closeButton = getByText('Close')
+    fireEvent.click(closeButton)
+
+    expect(menuButton).toBeTruthy()
   })
 })
