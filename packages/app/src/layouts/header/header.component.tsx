33ad21063b537be0ee7876dde5fa9c06aae4c20d
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-27 13:43:43 +0100

    Adds linkedin

--- packages/app/src/layouts/header/header.component.tsx
+++ packages/app/src/layouts/header/header.component.tsx
@@ -4,6 +4,7 @@ import Logo from '../../components/logo'
 import { Navigation, NavigationItem } from '../../components/navigation'
 
 const GITHUB_URL = 'https://github.com/covertbert/'
+const LINKEDIN_URL = 'https://www.linkedin.com/in/bertie-blackman-3654767a/'
 
 const Header: React.FC = () => (
   <div className="flex items-center bg-primary p-3 flex-wrap">
@@ -12,6 +13,7 @@ const Header: React.FC = () => (
       <NavigationItem to="/work" label="work" />
       <NavigationItem to="/blog" label="blog" />
       <NavigationItem external to={GITHUB_URL} label="github" />
+      <NavigationItem external to={LINKEDIN_URL} label="linkedin" />
     </Navigation>
   </div>
 )
