e91405170dae7b6bfe189c0e4567ac6acd6c30aa
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-28 22:13:16 +0100

    Refactors

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:cypress/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
