aa1e17c8ddfb83da371bfffe504c9227d2b582c8
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-29 16:24:48 +0100

    Splits form into components

--- src/components/form/form-submit.component.tsx
+++ src/components/form/form-submit.component.tsx
@@ -8,7 +8,7 @@ const FormSubmit: React.FC<FormSubmitProps> = ({ text }) => (
   <div className="flex items-center justify-between">
     <button
       className="bg-blue-500 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
-      // type="submit"
+      type="submit"
     >
       {text}
     </button>
