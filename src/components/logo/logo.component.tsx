a96618d29f94bdc442ef8edcb66283cb0e09add0
Bertie Blackman <blackmanrgh@gmail.com>
2020-05-30 19:23:13 +0100

    Ally logo button

--- src/components/logo/logo.component.tsx
+++ src/components/logo/logo.component.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Logo = () => (
-  <Link className="p-2 inline-flex items-center text-white text-2xl" to="/">
+  <Link aria-label="Home" className="p-2 inline-flex items-center text-white text-2xl" to="/">
     bertie.dev
   </Link>
 )
