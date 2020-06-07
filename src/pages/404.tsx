f8d937954934e165f3de1a5794c97f85f79add46
Bertie Blackman <blackmanrgh@gmail.com>
2020-06-07 21:03:07 +0100

    Fixes 404 flash

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { navigate } from 'gatsby'
 
-const App = () => (
-  <div className="absolute w-full h-full flex flex-col justify-center items-center bg-primary">
-    <h1>Page not found!</h1>
-    <h3>Redirecting to the home page</h3>
-  </div>
-)
+setTimeout(() => {
+  navigate('/')
+}, 1500)
+
+const browser = typeof window !== 'undefined' && window
+
+const App = () =>
+  browser && (
+    <div className="absolute w-full h-full flex flex-col justify-center items-center bg-primary">
+      <h1>Page not found!</h1>
+      <h3>Redirecting to the home page</h3>
+    </div>
+  )
 
 export default App
