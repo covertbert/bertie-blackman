5dbe50c80261094c92a61176586ac474a09e90c8
Bertie Blackman <blackmanrgh@gmail.com>
2020-06-07 21:00:36 +0100

    Styles 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const App = () => (
-  <>
-    <h1>Not found!</h1>
-  </>
+  <div className="absolute w-full h-full flex flex-col justify-center items-center bg-primary">
+    <h1>Page not found!</h1>
+    <h3>Redirecting to the home page</h3>
+  </div>
 )
 
 export default App
