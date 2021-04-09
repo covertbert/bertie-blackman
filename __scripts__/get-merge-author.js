943235226beec3cc291557dd6b9a26bbc9a95d41
Bertie Blackman <blackmanrgh@gmail.com>
2021-04-09 14:38:55 +0100

    Logs PR author

--- __scripts__/get-merge-author.js
+++ __scripts__/get-merge-author.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
 module.exports = ({ github }) => {
-  const prAuthorUsername = github.event.commits[0]?.author?.username
+  const prHasAuthor =
+    github.event.commits[0] &&
+    github.event.commits[0].author &&
+    github.event.commits[0].author.username
 
-  if (prAuthorUsername) {
+  if (prHasAuthor) {
+    const prAuthorUsername = github.event.commits[0].author.username
     core.setOutput('author', prAuthorUsername)
   }
 
